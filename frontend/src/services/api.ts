e52d35d85e5bad84d6e9472e1b08ddd3f3965f34
Yogesh bhosle <yogeshbhosle35@gmail.com>
2025-11-01 15:28:40 +0530

    Fix folder structure and rebuild backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://favorite-movies-webapp-2.onrender.com", // ✅ Use Render backend URL
 });
 
 export default api;
